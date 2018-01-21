fd77621bf206d358d54119c15b3937938c24e441
Unknown <hcv1027@gmail.com>
2018-01-21 21:17:33 +0800

    Update

---
+++ agent.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as import pdb
+
+from pysc2.agents import base_agent
+
+
+class SC2Agent(base_agent.BaseAgent):
+    def __init__(self):
+
+    def step(self, obs):
